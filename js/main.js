175fe0a03ae49d1d0cf1c94e4ba1c38c40d672a9
Max Josino <max.josino@gmail.com>
2019-06-19 16:12:08 +0200

    new try

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 /* ===================================================================
  * Main JS
  *
+<<<<<<< HEAD
  * ------------------------------------------------------------------- */
+=======
+ * ------------------------------------------------------------------- */ 
+>>>>>>> parent of f068ea7... cleaning the room
 
 (function($) {
 
 	"use strict";
 
+<<<<<<< HEAD
 	var cfg = {
 		defAnimation   : "fadeInUp",    // default css animation
 		scrollDuration : 800,           // smoothscroll duration
@@ -20,48 +25,101 @@
 	var ssPreloader = function() {
 
 		$WIN.on('load', function() {
+=======
+	var cfg = {		
+		defAnimation   : "fadeInUp",    // default css animation		
+		scrollDuration : 800,           // smoothscroll duration
+		statsDuration  : 4000           // stats animation duration
+	},	
+	$WIN = $(window);
+
+	
+	/* Preloader 
+	 * -------------------------------------------------- */
+	var ssPreloader = function() {
+
+		$WIN.on('load', function() {	
+>>>>>>> parent of f068ea7... cleaning the room
 
 			// force page scroll position to top at page refresh
 			$('html, body').animate({ scrollTop: 0 }, 'normal');
 
+<<<<<<< HEAD
 	      // will first fade out the loading animation
+=======
+	      // will first fade out the loading animation 
+>>>>>>> parent of f068ea7... cleaning the room
 	    	$("#loader").fadeOut("slow", function(){
 
 	        // will fade out the whole DIV that covers the website.
 	        $("#preloader").delay(300).fadeOut("slow");
 
+<<<<<<< HEAD
 	      });
 	  	});
 	}; 	
+=======
+	      }); 
+	  	});
+	}; 
+
+
+ 
+
+  
+	
+>>>>>>> parent of f068ea7... cleaning the room
 
   /* Intro Animation
 	* ------------------------------------------------------- */
 	var ssIntroAnimation = function() {
 
 		$WIN.on('load', function() {
+<<<<<<< HEAD
 
+=======
+		
+>>>>>>> parent of f068ea7... cleaning the room
 	     	if (!$("html").hasClass('no-cssanimations')) {
 	     		setTimeout(function(){
 	    			$('.animate-intro').each(function(ctr) {
 						var el = $(this),
+<<<<<<< HEAD
 	                   animationEfx = el.data('animate') || null;
 
 	               if (!animationEfx) {
 	                 	animationEfx = cfg.defAnimation;
+=======
+	                   animationEfx = el.data('animate') || null;		                                      
+
+	               if (!animationEfx) {
+	                 	animationEfx = cfg.defAnimation;	                 	
+>>>>>>> parent of f068ea7... cleaning the room
 	               }
 
 	              	setTimeout( function () {
 							el.addClass(animationEfx + ' animated');
 						}, ctr * 300);
+<<<<<<< HEAD
 					});
 				}, 100);
 	     	}
 		});
+=======
+					});						
+				}, 100);
+	     	} 
+		}); 
+>>>>>>> parent of f068ea7... cleaning the room
 
 	};
 
 
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> parent of f068ea7... cleaning the room
 
 
   /* Initialize
@@ -69,9 +127,18 @@
 	(function ssInit() {
 
 		ssPreloader();
+<<<<<<< HEAD
 		ssIntroAnimation();
 
 	})();
 
 
 })(jQuery);
+=======
+		ssIntroAnimation();		
+
+	})();
+ 
+
+})(jQuery);
+>>>>>>> parent of f068ea7... cleaning the room
